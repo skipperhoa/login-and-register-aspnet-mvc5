2eef7b46d7fece059fee1b1e4f0deebe75a0d533
unknown <thanhhoacth2013@gmail.com>
2020-04-16 17:06:47 +0700

    2020-04-16

---
+++ Models/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace LoginAndRegisterASPMVC5.Models
+{
+    public class UserLogin
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
